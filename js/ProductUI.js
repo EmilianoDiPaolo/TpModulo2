feced8658440eb6333e06f80f0deb4a548d11d2a
EmilianoDiPaolo <emidipaolo2003@gmail.com>
2026-09-16 12:37:53 -0300

    implemento el constructor de ProducUI, hago funciones de limpiar, boton y empiezo crearitem

--- js/ProductUI.js
+++ js/ProductUI.js
@@ -1,7 +1,35 @@
 export class ProductUi {
   constructor(manager) {
     this.manager = manager;
+
+    this.nombre = document.getElementById("nombreProducto");
+    this.categoria = document.getElementById("categoriaProducto");
+    this.precio = document.getElementById("precioProducto");
+    this.stock = document.getElementById("stockProducto");
+    this.btnAgregar = document.getElementById("agregarProducto");
+    this.buscarProducto = document.getElementById("buscarProducto");
+    this.listado = document.getElementById("listadoProductos");
+  }
+
+  crearItemProducto(producto) {
+    const item = document.createElement("li");
+    item.className = "col-md-6 col-lg-4";
+  }
+
+  crearBoton(texto, clase) {
+    const btn = document.createElement("button");
+    btn.textContent = texto;
+    btn.className = clase;
+
+    return btn;
   }
 
   render() {}
+
+  limpiarFormulario() {
+    this.nombre.value = "";
+    this.categoria.value = "";
+    this.precio.value = "";
+    this.stock.value = "";
+  }
 }
